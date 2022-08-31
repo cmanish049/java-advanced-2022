43f832b0e34ab107745c558b0a2e751276504f3a
Manish Chaudhary <cmanish049@gmail.com>
2022-08-31 08:26:06 +0545

    window event listerner using interface

---
+++ WindowEventListenerUsingInterfaceDemo.java
@@ -0,0 +1,54 @@
+import java.awt.event.*;
+import java.awt.Frame;
+
+public class WindowEventListenerUsingInterfaceDemo implements WindowListener {
+
+    public Frame f;
+    
+    public static void main(String[] args) {
+        WindowEventListenerUsingInterfaceDemo weld = new WindowEventListenerUsingInterfaceDemo();
+        weld.prepareWindow();
+    }
+    
+    private void prepareWindow() {
+        f = new Frame();
+        f.setSize(400, 400);
+        f.addWindowListener(this); // I missed this in class, this is an important code, always rememeber to write this line, It registers the listener to the component or class itself.
+        f.setVisible(true);
+    }
+    @Override
+    public void windowOpened(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        f.dispose();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        
+    }
+    
+}
